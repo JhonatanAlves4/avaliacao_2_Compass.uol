b71a708bb02998316e579d928c1d1d05afb7ccc2
Jhonatan Alves Pereira <71884829+JhonatanAlves4@users.noreply.github.com>
2022-06-19 21:21:37 -0300

    Questão 3 finalizada.

---
+++ Questao_3/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Insira sua mensagem");
+		String message = scanner.nextLine();
+
+		String contente = ":-)";
+		String chateado = ":-(";
+
+		int contenteValue = 0;
+		int chateadoValue = 0;
+
+		for (int i = 0; i < message.length(); i++) {
+			if (message.substring(i).startsWith(contente)) {
+				contenteValue++;
+			} else if (message.substring(i).startsWith(chateado)) {
+				chateadoValue++;
+			}
+		}
+
+		System.out.println();
+		if (contenteValue == chateadoValue) {
+			System.out.println("Neutro");
+		} else if (contenteValue > chateadoValue) {
+			System.out.println("Divertido");
+		} else if (chateadoValue > contenteValue) {
+			System.out.println("Chateado");
+		}
+
+	}
+
+}
